1e7a52098c23e765a90514b38addb06ae80b3b7c
Elvin FICHOT--CORGERON <elvin.fichot-corgeron@iut-dijon.u-bourgogne.fr>
2024-03-20 23:52:53 +0100

    feat(realisations) : add my project

---
+++ portfolio_Elvin/src/components/realisation/Projet.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="flex justify-center">
+    <div class="card w-96 bg-secondary shadow-xl text-neutral-content m-8">
+      <div class="card-body">
+        <h2 class="card-title">
+          {{ props.realisation.title }}
+        </h2>
+        <p>{{ props.realisation.description }}</p>
+        <button class="btn btn-primary" v-if="props.realisation.link">
+          <a :href="props.realisation.link">Lien vers le projet</a>
+        </button>
+
+        <div class="card-actions justify-end">
+          <div
+            class="badge badge-outline"
+            v-for="tech in props.realisation.stack"
+          >
+            {{ tech }}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Realisation } from "../../data/realisation";
+interface Props {
+  realisation: Realisation;
+}
+const props = defineProps<Props>();
+</script>
--- portfolio_Elvin/src/pages/RealisationsPage.vue
+++ portfolio_Elvin/src/pages/RealisationsPage.vue
@@ -1,3 +1,11 @@
 <template>
   <h1 class="text-3xl font-bold text-primary p-4">Mes Réalisations</h1>
+  <div>
+    <Projet v-for="realisation in realisations" :realisation="realisation" />
+  </div>
 </template>
+
+<script setup lang="ts">
+import Projet from "../components/realisation/Projet.vue";
+import { realisations } from "../data/realisation.ts";
+</script>
