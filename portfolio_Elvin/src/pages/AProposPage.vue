96795e2fb683a2e96b4ae035e93140e6a3ce30a8
Elvin FICHOT--CORGERON <elvin.fichot-corgeron@iut-dijon.u-bourgogne.fr>
2024-06-17 15:28:35 +0200

    Page à propos

--- portfolio_Elvin/src/pages/AProposPage.vue
+++ portfolio_Elvin/src/pages/AProposPage.vue
@@ -1,3 +1,34 @@
 <template>
   <h1 class="text-3xl font-bold text-primary p-4">A propos de moi</h1>
+  <div class="grid grid-cols-3 gap-8 m-8">
+    <div class="justify-center">
+      <div class="avatar">
+        <div class="rounded-xl">
+          <img src="../../public/photo.PNG" />
+        </div>
+      </div>
+    </div>
+    <div>
+      Je suis Elvin FICHOT--CORGERON développeur junior spécialisé dans le web
+      Depuis mon plus jeune âge je m'intérresse aux nouvelles technologies, je
+      me suis naturellement tourné vers l'informatique et le développement, ce
+      qui ma amené à faire mon stage de 3ème afin d'observer le métier de
+      développeur et conforter mon souhait de m'orienter dans cette voie. En
+      décidant de poursuivre mes études à l'IUT informatique de Dijon j'ai eu
+      l'occasion de découvrir diverses technologies en le mettant en oeuvres
+      dans différentes situations tel que des TP, des projets ou bien des
+      évenements comme les 24h des IUT. J'ai également pu me former lors
+      d'experiences personnelles ou professionnelles.
+    </div>
+    <div>
+      Je suis également un grand fan de sport, j'ai nottament pratiqué plusieurs
+      année la natation en compétition avec un niveau régional. Mais depuis peu
+      je me suis lancé dans la course pour essayer de relevé des défis. J'ai
+      couru le semi-marathon de Paris et je me prépare actuellement à courir le
+      marathon de Paris. Cela me permet de me recentrer sur moi même et vivre
+      des moments incroyables. Je vais également participer aux Jeux Olympiques
+      de Paris 2024 au stade nautique de Vaires sur Marne en tant qu'équipier
+      stadier.
+    </div>
+  </div>
 </template>
